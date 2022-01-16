36c68a24cbe71dcadbd7b108921b1204897b6fef
Hmmf <97818440+Hmmf@users.noreply.github.com>
2022-01-16 16:07:49 +1200

    Add files via upload

---
+++ 3/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+
+bool dotCheck (string ip)
+{
+    int i = 0, c = 0;
+    while (i != -1 && i < ip.size())
+    {
+
+        i = ip.find('.' , i + 1);
+        c++;
+        if (i == 0 || i == ip.size() - 1 ||  ip[i - 1] == '.' || ip[i + 1] == '.')
+            return false;
+    }
+    if (c != 4) return false;
+    else return true;
+}
+
+
+int correctIp(string ip)
+{
+    int i, c;
+    if (!dotCheck(ip))
+        return false;
+    int dotPos = 0;
+    for (int j = 0; j < 4; j++)
+    {
+        i = 0;
+        c = 0;
+        while (ip[dotPos + i] != '.' && dotPos + i < ip.size())
+        {
+            if (ip[dotPos + i] >= '0' && ip[dotPos + i] <= '9')
+            {
+                c = c * 10 + ip[dotPos + i] - '0';
+                if (c == 0 && ip[dotPos + i + 1] != '.' && dotPos + i + 1 < ip.size())
+                    return false;
+                i++;
+            }
+            else return false;
+        }
+        if (c > 255)
+            return false;
+        dotPos += i + 1;
+    }
+    return true;
+}
+
+
+int main()
+{
+    string ip;
+    cout << "Input your ip: ";
+    cin >> ip;
+    if (correctIp(ip))
+        cout << "Yes";
+    else cout << "No";
+    return 0;
+}
